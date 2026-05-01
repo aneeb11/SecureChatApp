cff8aba270f98c4abe4719d1725fd0f9f3c7625a
aneeb11 <aneeburrehman135@gmail.com>
2026-05-01 17:14:13 +0500

    Week 1: Server socket connection and message receiving logic

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,14 +1,34 @@
 package controller;
-import com.sun.tools.javac.Main;
-
 import java.io.*;
 import java.net.*;
+
 
 public class Server {
     public static final int PORT = 5000;
     public static void main(String[] args) {
-        System.out.println("Server Starting on Port : "+PORT);
-
+        System.out.println("Server Starting on Port : "+ PORT);
+        try {
+            ServerSocket server_socket = new ServerSocket(PORT);
+            System.out.println("✅ Server started! Waiting for client...");
+            Socket client_socket = server_socket.accept();
+            System.out.println("✅ Client connected!");
+            BufferedReader reader = new BufferedReader
+                    (new InputStreamReader(client_socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(client_socket.getOutputStream(),true);
+            String message;
+            while((message = reader.readLine())!=null)
+            {
+                System.out.println("Client Says : " + message);
+                writer.println("Server Received : " + message);
+                if(message.equalsIgnoreCase("exit"))
+                {
+                    System.out.println("Client Disconnected.");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error : " +e.getMessage());
+        }
 
     }
 }

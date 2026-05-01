a464208a1b86d40003d509722cd9eebca045e6b7
aneeb11 <aneeburrehman135@gmail.com>
2026-05-01 15:59:43 +0500

    Week 1 : Added User,Message,Server base classes

---
+++ src/controller/Server.java
@@ -0,0 +1,14 @@
+package controller;
+import com.sun.tools.javac.Main;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static final int PORT = 5000;
+    public static void main(String[] args) {
+        System.out.println("Server Starting on Port : "+PORT);
+
+
+    }
+}

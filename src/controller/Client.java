c0d37cc0828adfe5b511abd65893287abb18be5c
aneeb11 <aneeburrehman135@gmail.com>
2026-05-02 14:39:14 +0500

    Week 1 : Client Class Updated

--- src/controller/Client.java
+++ src/controller/Client.java
@@ -1,6 +1,7 @@
 package controller;
 import java.io.*;
 import java.net.*;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Client {
@@ -17,6 +18,26 @@ public class Client {
             PrintWriter ServerWriter =
                     new PrintWriter(Socket.getOutputStream());
             Scanner keyboard = new Scanner(System.in);
+            Thread listenThread = new Thread(() -> {
+                try {
+                    String response;
+                    while ((response = ServerReader.readLine()) != null) {
+                        System.out.println("Server: " + response);
+                        System.out.print("You: ");
+                    }
+                } catch (IOException e) {
+                    System.out.println("Disconnected from server.");
+                }
+            });
+            listenThread.start();
+            System.out.println("You : ");
+            while (keyboard.hasNextLine())
+            {
+                String message = keyboard.nextLine();
+                ServerWriter.println(message);
+                if(message.equalsIgnoreCase("exit"))break;
+                System.out.println("You : ");
+            }
 
         }
         catch (IOException e)

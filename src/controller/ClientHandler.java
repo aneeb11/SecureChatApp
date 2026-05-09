295693f90450e4c8711fc3e332bb7311fc3ffc4f
aneeb11 <aneeburrehman135@gmail.com>
2026-05-09 15:33:52 +0500

    Week 2 : Client Handler Added

--- src/controller/ClientHandler.java
+++ src/controller/ClientHandler.java
@@ -5,6 +5,7 @@ import java.net.*;
 public class ClientHandler implements Runnable{
     private Socket client_socket;
     private String client_name;
+    private PrintWriter writer;
     public ClientHandler(Socket client_socket)
     {
         this.client_socket=client_socket;
@@ -13,7 +14,7 @@ public class ClientHandler implements Runnable{
     public void run() {
         try{
             BufferedReader reader = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(client_socket.getOutputStream(),true);
+            writer = new PrintWriter(client_socket.getOutputStream(),true);
             writer.println("Connected to Server! Enter your name : ");
             client_name=reader.readLine();
             System.out.println("✅ " + client_name + " joined the chat!");
@@ -23,7 +24,7 @@ public class ClientHandler implements Runnable{
                 System.out.println(client_name+ ": "+ message);
                 if (message.equalsIgnoreCase("exit"))
                 {
-                    System.out.println(client_name+" lef the chat. ");
+                    System.out.println(client_name+" left the chat. ");
                     break;
                 }
             }
@@ -32,6 +33,17 @@ public class ClientHandler implements Runnable{
         catch (IOException e)
         {
             System.out.println("❌ Error : "+e.getMessage());
+        }
+    }
+    public void broadcast(String message)
+
+    {
+        for(ClientHandler client : Server.clients)
+        {
+            if(client!=this)
+            {
+                client.writer.println(message);
+            }
         }
     }
 }
--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,10 +1,12 @@
 package controller;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 
 public class Server {
     public static final int PORT = 5000;
+    public static List <ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Server Starting on Port : " + PORT);
